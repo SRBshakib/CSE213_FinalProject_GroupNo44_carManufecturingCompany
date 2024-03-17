b60f89bcb3e519e5aa9afee57f0dc16c1239481e
Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>
2024-03-17 17:47:13 +0600

    by srb

---
+++ CarManufecturingCompany/src/mainpkg/UserType.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mainpkg;
+
+/**
+ *
+ * @author SRB Shakib
+ */
+public enum UserType {
+    PRODUCTION_MANAGER,
+    SALES_MANAGER,
+    CUSTOMER,
+}
