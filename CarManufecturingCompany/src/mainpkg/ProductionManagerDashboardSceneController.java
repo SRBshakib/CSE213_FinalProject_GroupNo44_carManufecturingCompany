3e6a662e67ef0e3585777531bda40e23e7653e51
Rifat21c <157726414+Rifat21c@users.noreply.github.com>
2024-03-17 17:29:49 +0600

    Rifat (#25)
    * Create ProductionManagerDashboardScene.fxml
    
    * Create .gitignore
    
    * new update file
    
    * main file
    
    * ignore
    
    * time by rifat
    
    * by rifat
    
    * Revert "by rifat"
    
    This reverts commit e154d0147ecf208e37bbd34382c15c7602f9ccce.
    
    * by rifat
    
    * by rifat
    
    * by rifat
    
    ---------
    
    Co-authored-by: Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>

---
+++ CarManufecturingCompany/src/mainpkg/ProductionManagerDashboardSceneController.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Lenovo
+ */
+public class ProductionManagerDashboardSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void carModelButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void working_Schedule_Button_On_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void upcomingCarModelsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void assemblyManuelButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void carLunchingEventButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void assignTasksButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void carDesignButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void issueButtonOnClick(ActionEvent event) {
+    }
+    
+}
