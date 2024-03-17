a11d84a464c827e97cf7bf7d0d27aeb68c4d57a5
Shakibur Rahman Bhuiyan <66984277+SRBshakib@users.noreply.github.com>
2024-03-17 23:14:49 +0600

    Shakib (#22)
    * AssemblyLinneWorker by SRBshakib
    
    * Delete FinanceManagerDashboardScene.fxml
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by Shakib
    
    * by srb
    
    * shakib

--- CarManufecturingCompany/src/mainpkg/SignUpSceneController.java
+++ CarManufecturingCompany/src/mainpkg/SignUpSceneController.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
@@ -25,8 +26,6 @@ public class SignUpSceneController implements Initializable {
     @FXML
     private PasswordField userPasswordPwfield;
     @FXML
-
-
     private ComboBox<String> userTypeComboBox;
 
    
