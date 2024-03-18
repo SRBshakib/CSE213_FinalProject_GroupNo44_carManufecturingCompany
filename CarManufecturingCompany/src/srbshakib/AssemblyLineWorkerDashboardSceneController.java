44fc583da2f12fe2812794fad56f22e2e113c503
Shakibur Rahman Bhuiyan <66984277+SRBshakib@users.noreply.github.com>
2024-03-19 01:55:21 +0600

    Assembly Line Worker Dashboard update (#36)
    * AssemblyLinneWorker by SRBshakib
    
    * Delete FinanceManagerDashboardScene.fxml
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by Shakib
    
    * by srb
    
    * shakib
    
    * by srbshakib
    
    * by srbshakib
    
    * by shakib

---
+++ CarManufecturingCompany/src/srbshakib/AssemblyLineWorkerDashboardSceneController.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package srbshakib;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author SRB Shakib
+ */
+public class AssemblyLineWorkerDashboardSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void homeButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void assignedTaskButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void inventoryButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void instructionsButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void leaveApplicationButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void traingButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void flagReportButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void checkScheduleButtonOnMuseClicked(ActionEvent event) {
+    }
+    
+}
