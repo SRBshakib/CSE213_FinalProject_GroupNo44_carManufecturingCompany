06bd0219ed4fe40b5e76bc64357c484988bc4a4a
Shakibur Rahman Bhuiyan <66984277+SRBshakib@users.noreply.github.com>
2024-03-20 01:40:42 +0600

    Dip (#42)
    * added dashboard, meeting class, terms andconditions
    
    marge kor
    
    * update tester xml
    
    * update
    
    * add fxml of all
    
    * update tester fxml and ADD BIN FILE
    
    * Revert "Shakib (#40)"
    
    This reverts commit cab950fb2cddd7f1dedaac610c642883b9ca73fb.
    
    ---------
    
    Co-authored-by: DIPAYON <DIPAYON@192.168.0.104>
    Co-authored-by: Dip Kundu <102429238+dip-ayon@users.noreply.github.com>

---
+++ CarManufecturingCompany/src/mainpkg/HR_TermsAndConditionsReadSceneController.java
@@ -0,0 +1,39 @@
+package mainpkg;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HR_TermsAndConditionsReadSceneController implements Initializable {
+
+    @FXML
+    private Label termsAndConditionsSetTextLabel;
+       
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void loadButtonOnClick(ActionEvent event) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("termsAndConditions.bin"))) {
+            String loadedData = (String) ois.readObject();
+            termsAndConditionsSetTextLabel.setText(loadedData);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    
+}
