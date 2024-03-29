5deb993243913adec948c3deccb6f585f978b000
Shakibur Rahman Bhuiyan <66984277+SRBshakib@users.noreply.github.com>
2024-03-29 17:26:09 +0600

    Shakib (#61)
    * AssemblyLinneWorker by SRBshakib
    
    * Delete FinanceManagerDashboardScene.fxml
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by shakib
    
    * by Shakib
    
    * by srb
    
    * shakib
    
    * by srbshakib
    
    * by srbshakib
    
    * by shakib
    
    * by shakib
    
    * Update AssemblyLineWorkerDashboardScene.fxml
    
    * update
    
    * by shakib
    
    * Update Tester.fxml
    
    * Update TesterController.java
    
    * Update AssemblyLineWorkerDashboardScene.fxml
    
    * by shakib
    
    * update
    
    * update
    
    * by shakib
    
    * Create AssignedTask.java
    
    * Update built-jar.properties
    
    * by shakib
    
    * by shakib

---
+++ CarManufecturingCompany/src/srbshakib/SupplyChainManager/SupplyChainManagerDashboardSceneController.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package srbshakib.SupplyChainManager;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author SRB Shakib
+ */
+public class SupplyChainManagerDashboardSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void homeButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("SupplyChainManagerDashboardScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
+    }
+
+    @FXML
+    private void trackLogisticsButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void orderButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void distribruteButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void orderHistoryButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateInventoryButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("MakeInventoryScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
+    }
+
+    @FXML
+    private void customerOrderButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void supplierListButtonOnMuseClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void supplyHistoryButtonOnMuseClicked(ActionEvent event) {
+    }
+    
+}
