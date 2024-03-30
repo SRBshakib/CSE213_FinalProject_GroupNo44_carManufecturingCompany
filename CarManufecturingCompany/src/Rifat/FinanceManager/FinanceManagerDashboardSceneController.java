a6d0d48cf44b690306b00a548aece69948946b9c
Rifat21c <157726414+Rifat21c@users.noreply.github.com>
2024-03-30 20:48:34 +0600

    Rifat (#69)
    * Create ProductionManagerDashboardScene.fxml
    
    * Create .gitignore
    
    * new update file
    
    * main file
    
    * ignore
    
    * time by rifat
    
    * by rifat
    
    * Revert "by rifat"
    
    This reverts commit e154d0147ecf208e37bbd34382c15c7602f9ccce.
    
    * by rifat
    
    * by rifat
    
    * by rifat
    
    * by rifat
    
    * by rifat
    
    * by rifat
    
    * ignore
    
    * by rifat
    
    * rifat
    
    ---------
    
    Co-authored-by: Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>

--- CarManufecturingCompany/src/Rifat/FinanceManager/FinanceManagerDashboardSceneController.java
+++ CarManufecturingCompany/src/Rifat/FinanceManager/FinanceManagerDashboardSceneController.java
@@ -2,7 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
+ Rifat
+package Rifat.FinanceManager;
+
 package mainpkg;
+ main
 
 import java.net.URL;
 import java.util.ResourceBundle;
