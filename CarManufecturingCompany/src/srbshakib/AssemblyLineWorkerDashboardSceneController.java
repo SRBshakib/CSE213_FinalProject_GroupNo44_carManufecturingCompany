66cef61e961d51e04e8d69e25c6d2763e738ad7a
AuntiSaha <157971697+AuntiSaha@users.noreply.github.com>
2024-03-30 15:16:28 +0600

    Aunti (#65)
    * update main file
    
    * download
    
    * customer and supplier class
    
    * supplier dashboard
    
    * By Aunti
    
    * by Aunti
    
    * Create OrderListScene.fxml
    
    ---------
    
    Co-authored-by: Shakibur Rahman Bhuiyan <66984277+SRBshakib@users.noreply.github.com>

--- CarManufecturingCompany/src/srbshakib/AssemblyLineWorkerDashboardSceneController.java
+++ CarManufecturingCompany/src/srbshakib/AssemblyLineWorkerDashboardSceneController.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
@@ -79,3 +80,4 @@ public class AssemblyLineWorkerDashboardSceneController implements Initializable
 
     
 }
+
