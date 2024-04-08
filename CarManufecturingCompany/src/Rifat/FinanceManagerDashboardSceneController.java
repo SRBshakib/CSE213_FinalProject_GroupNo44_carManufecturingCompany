d358c0b32c9291e8b968c4a5e1189a2a29a00e47
Rifat21c <157726414+Rifat21c@users.noreply.github.com>
2024-04-09 01:37:49 +0600

    Reapply "rifat" (#115)
    This reverts commit 335814f9b73a1324e14b244125179720c4aaf644.

--- CarManufecturingCompany/src/Rifat/FinanceManagerDashboardSceneController.java
+++ CarManufecturingCompany/src/Rifat/FinanceManagerDashboardSceneController.java
@@ -6,11 +6,17 @@
 package Rifat;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -28,31 +34,71 @@ public class FinanceManagerDashboardSceneController implements Initializable {
     }    
 
     @FXML
-    private void paymentButtonOnClicked(ActionEvent event) {
+    private void homeButtonOnMuseClicked(ActionEvent event) {
     }
 
     @FXML
-    private void budgetButtonOnClicked(ActionEvent event) {
+    private void paymentButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("PaymentScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
     @FXML
-    private void auditButtonOnClicked(ActionEvent event) {
+    private void transactionButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("TransactionScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
     @FXML
-    private void transactionButtonOnClicked(ActionEvent event) {
+    private void budgetButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("BudgetScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
     @FXML
-    private void financialReportButtonOnClicked(ActionEvent event) {
+    private void financialReportButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("FinancialReportScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
     @FXML
-    private void dealerInfoButtonOnClicked(ActionEvent event) {
+    private void salaryDistrubutionButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("SalaryDistributionScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
     @FXML
-    private void salaryDistributionButtonOnClicked(ActionEvent event) {
+    private void dealerInfoButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("DealerInfoScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
+    }
+
+    @FXML
+    private void auditButtonOnMuseClicked(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("AuditScene.fxml"));
+        Scene scene1 = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
+        
     }
 
     @FXML
