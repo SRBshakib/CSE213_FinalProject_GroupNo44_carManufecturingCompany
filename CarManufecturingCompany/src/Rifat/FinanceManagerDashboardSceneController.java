911e9e3fd4d7a6a5a18e40cda010fa645be7360d
Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>
2024-04-10 01:26:38 +0600

    update

--- CarManufecturingCompany/src/Rifat/FinanceManagerDashboardSceneController.java
+++ CarManufecturingCompany/src/Rifat/FinanceManagerDashboardSceneController.java
@@ -75,6 +75,10 @@ public class FinanceManagerDashboardSceneController implements Initializable {
     }
 
 
+<<<<<<< Updated upstream
+=======
+    @FXML
+>>>>>>> Stashed changes
 
     private void salaryDistrubutionButtonOnMuseClicked(ActionEvent event) throws IOException {
         Parent mainParent = FXMLLoader.load(getClass().getResource("SalaryDistributionScene.fxml"));
