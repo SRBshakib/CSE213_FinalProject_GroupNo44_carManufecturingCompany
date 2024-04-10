204ef70abecf7e2b30bc05abfee881f7f6f44743
Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>
2024-04-11 03:35:05 +0600

    supplier order

--- CarManufecturingCompany/src/srbshakib/SupplyChainManager/SupplierOrderSceneController.java
+++ CarManufecturingCompany/src/srbshakib/SupplyChainManager/SupplierOrderSceneController.java
@@ -4,15 +4,21 @@
  */
 package srbshakib.SupplyChainManager;
 
+import Dip.AppendableObjectOutputStream;
 import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import srbshakib.SupplyChainManager.SupplierInformation;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,7 +30,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -41,13 +50,37 @@ public class SupplierOrderSceneController implements Initializable {
     @FXML
     private TextField quantityTextField;
     @FXML
-    private DatePicker estimetedDateDatePicker;
+    private ComboBox<String> paymentComboBox;
+    @FXML
+    private TableView<OrderForSuppiler> orderInfoTableView;
+    @FXML
+    private TableColumn<OrderForSuppiler, Integer> orderCodeTableColumn;
+    @FXML
+    private TableColumn<OrderForSuppiler, String> supplierNameTableColumn;
+    @FXML
+    private TableColumn<OrderForSuppiler, String> productTableColumn;
+    @FXML
+    private TableColumn<OrderForSuppiler, Integer> quantityTableColumn;
+    @FXML
+    private TableColumn<OrderForSuppiler, String> paymentTableColumn;
 
-    /**
-     * Initializes the controller class.
-     */
+   private static int idCounter = 1110000;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        productComboBox.getItems().addAll("Engine Block", "Cylinder Head", "Pistons", "Crankshaft", "Camshaft", "Connecting Rods",
+    "Flywheel", "Driveshaft", "Differential", "Engine Control Unit (ECU)", "Transmission Control Unit (TCU)","Electric Motor", "Battery Pack", "Inverter", "Transmission","Steel Sheets (Body Panels)", "Aluminum Castings (Engine Hood, Trunk Lid)", "Composite Materials (Bumpers, Fenders)",
+    "Windshield", "Side Windows", "Rear Window (Safety Glass)", "Sunroof (Optional)", "Doors", "Hood", "Trunk Lid",
+    "Hinges", "Latches", "Locks", "Frame Rails", "Subframe Components (Steel)",    "Steering Wheel", "Steering Column", "Rack and Pinion Steering Gear", "Tie Rods", "Drag Link",
+    "MacPherson Struts", "Coil Springs", "Shock Absorbers", "Control Arms", "Bushings", "Ball Joints","Disc Brakes (Front, Rear) or Drum Brakes (Rear, Less Common in Modern Cars)", "Brake Pads", "Rotors (Disc Brakes)", "Drums (Drum Brakes)",
+    "Calipers", "Master Cylinder", "Brake Lines", "Brake Fluid","Steel Wheels or Aluminum Alloy Wheels", "Tires (Specified Size and Type - Summer, All-Season, Winter)",
+    "Hubs", "Lug Nuts", "Seats (Frames, Upholstery)", "Instrument Panel, Dashboard", "Steering Wheel Cover, Gear Shift Knob (Interior Trim)",
+    "Door Panels", "Carpeting", "Headliner","Battery", "Alternator", "Starter Motor", "Wiring Harness", "Fuses", "Relays", "Light Switches", "Window Switches",
+    "Headlights", "Taillights", "Brake Lights", "Turn Signals (Exterior Lighting)", "Instrument Cluster Gauges", "Infotainment System (Interior Electronics)", "Compressor", "Condenser", "Evaporator (AC System)", "Heater Core", "Blower Motor","Airbags (Driver, Passenger, Side-Impact)", "Seatbelts (Pre-Tensioners, Force Limiters)", "Anti-Lock Braking System (ABS)",
+    "Electronic Stability Control (ESC)", "Traction Control System (TCS)", "Tire Pressure Monitoring System (TPMS)","Fasteners (Bolts, Nuts, Screws, Washers)", "Gaskets & Seals (Engine Gaskets, Transmission Gaskets, Door Seals)",
+    "Filters (Air Filter, Oil Filter, Fuel Filter)", "Fluids (Engine Oil, Transmission Fluid, Coolant, Brake Fluid)",
+    "Exhaust System (Catalytic Converter, Muffler, Tailpipe)");
+        
+        paymentComboBox.getItems().addAll("Done", "Due");
        
     }    
 
@@ -158,6 +191,63 @@ public class SupplierOrderSceneController implements Initializable {
     }
     @FXML
     private void orderButtonOnMouseClicked(ActionEvent event) {
+        OrderForSuppiler i = new OrderForSuppiler(
+                
+                generateUniqueId(),
+                suppilerComboBox.getValue(),
+                productComboBox.getValue(),
+                paymentComboBox.getValue(),
+                Integer.parseInt(quantityTextField.getText())
+                
+                
+                
+        );
+        
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        File f = null;
+        try {
+            f = new File("OrderForSuppiler.bin");
+            if (f.exists()) {
+                fos = new FileOutputStream(f, true);
+                oos = new AppendableObjectOutputStream(fos);
+            } else {
+                fos = new FileOutputStream(f);
+                oos = new ObjectOutputStream(fos);
+            }
+
+            oos.writeObject(i);
+
+        } catch (IOException ex) {
+            Logger.getLogger(SupplierOrderSceneController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(SupplierOrderSceneController.class
+                        .getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+                suppilerComboBox.setValue(null);
+                productComboBox.setValue(null);
+                paymentComboBox.setValue(null);
+                quantityTextField.clear();
+    }
+     private int generateUniqueId() {
+        return ++idCounter; // Increment the ID counter and return
+    }
+
+    private void saveSupplierInformation(SupplierInformation supplier) {
+        try (FileOutputStream fos = new FileOutputStream("SupplierList.bin", true);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(supplier);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
       
     }
 
@@ -184,6 +274,48 @@ public class SupplierOrderSceneController implements Initializable {
 
     // Now you can use 'supplierNames' to set the items of your ComboBox
     suppilerComboBox.setItems(supplierNames);
+    }
+
+    @FXML
+    private void loadOrderInfoButtonOnMouseClicked(ActionEvent event) {
+        ObservableList<OrderForSuppiler> supplierOrderListInfo = FXCollections.observableArrayList();
+        
+        orderCodeTableColumn.setCellValueFactory(new PropertyValueFactory<>("orderCode"));
+        supplierNameTableColumn.setCellValueFactory(new PropertyValueFactory<OrderForSuppiler,String>("supplierName"));
+        productTableColumn.setCellValueFactory(new PropertyValueFactory<OrderForSuppiler,String>("productName"));
+        quantityTableColumn.setCellValueFactory(new PropertyValueFactory<OrderForSuppiler, Integer>("quantity"));
+        paymentTableColumn.setCellValueFactory(new PropertyValueFactory<OrderForSuppiler, String>("payment"));
+        
+        
+        File f = null;
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+
+        try {
+            f = new File("OrderForSuppiler.bin");
+            fis = new FileInputStream(f);
+            ois = new ObjectInputStream(fis);
+            OrderForSuppiler p;
+            try {
+                while (true) {
+                    p = (OrderForSuppiler) ois.readObject();
+                    supplierOrderListInfo.add(p);
+                    System.out.println(p.toString());
+                }
+            } catch (Exception e) {
+            }
+        } catch (IOException ex) {
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException ex) {
+            }
+
+        }
+        orderInfoTableView.setItems(supplierOrderListInfo);
+        System.out.println(supplierOrderListInfo.toString());
     }
     
     }
