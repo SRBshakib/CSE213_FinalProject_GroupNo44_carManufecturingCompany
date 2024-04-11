e280e7340587a326337782e3c1f517ec59692746
DIPAYON <DIPAYON@DIPAYON>
2024-04-11 23:40:51 +0600

    by dip

---
+++ CarManufecturingCompany/src/mainpkg/signUpData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainpkg;
+
+/**
+ *
+ * @author DIPAYON
+ */
+public class signUpData {
+    private int id;
+    private String email, password, userType;
+    
+}
