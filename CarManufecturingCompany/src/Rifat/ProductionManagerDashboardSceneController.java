205a2c3e7ccd660aa7bc3b8a3ea46740a0b6b550
Rifat21c <157726414+Rifat21c@users.noreply.github.com>
2024-04-15 18:08:41 +0600

    rifat

--- CarManufecturingCompany/src/Rifat/ProductionManagerDashboardSceneController.java
+++ CarManufecturingCompany/src/Rifat/ProductionManagerDashboardSceneController.java
@@ -4,7 +4,11 @@
  */
 package Rifat;
 
+import Dip.MeetingSchedule;
+import Dip.Notice;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -14,6 +18,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 /**
@@ -23,11 +29,18 @@ import javafx.stage.Stage;
  */
 public class ProductionManagerDashboardSceneController implements Initializable {
 
+    @FXML
+    private ComboBox<String> NoticeUserSelectCB;
+    @FXML
+    private TextArea noticeOutputTA;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        NoticeUserSelectCB.getItems().addAll("Managing Director",
+                "Human Resource Manager");
         // TODO
     }    
 
@@ -103,5 +116,67 @@ public class ProductionManagerDashboardSceneController implements Initializable 
         window.setScene(scene1);
         window.show();
     }
-    
+
+    @FXML
+    private void refreshNoticeButtonOnClick(ActionEvent event) {
+        if (NoticeUserSelectCB.getValue().equals("Managing Director")){
+        ObjectInputStream ois = null;
+        try {
+            MeetingSchedule s;
+            FileInputStream fis = new FileInputStream("Schedule.bin");
+            ois = new ObjectInputStream(fis);
+
+            noticeOutputTA.setText(null);
+
+            while (true) {
+                s = (MeetingSchedule) ois.readObject();
+
+                noticeOutputTA.appendText(s.toString() + "\n");
+            }
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+
+        } catch (Exception ex) {
+
+            try {
+                System.out.println(ex.toString());
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException ex1) {
+            }
+        }
+    }else{
+        ObjectInputStream ois = null;
+        try {
+            Notice s;
+            FileInputStream fis = new FileInputStream("Notice.bin");
+            ois = new ObjectInputStream(fis);
+
+            noticeOutputTA.setText(null);
+
+            while (true) {
+                s = (Notice) ois.readObject();
+
+                noticeOutputTA.appendText(s.toString() + "\n");
+            }
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+
+        } catch (Exception ex) {
+
+            try {
+                System.out.println(ex.toString());
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException ex1) {
+            }
+        }  
+        }
 }
+    }
+    
+
