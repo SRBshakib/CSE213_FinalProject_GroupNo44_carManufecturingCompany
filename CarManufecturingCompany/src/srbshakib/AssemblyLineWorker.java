5a289d2c889352c48e7a08db908a0f7e2f80a172
Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>
2024-04-15 18:34:38 +0600

    Update AssemblyLineWorker.java

--- CarManufecturingCompany/src/srbshakib/AssemblyLineWorker.java
+++ CarManufecturingCompany/src/srbshakib/AssemblyLineWorker.java
@@ -18,8 +18,6 @@ public class AssemblyLineWorker extends Employee implements Serializable {
 
     public AssemblyLineWorker(LocalDate dateOfJoin, float salary, String designation, String name, int UserId, String password, String userType, String email, int phone, String address, LocalDate DoB) {
         super(dateOfJoin, salary, designation, name, UserId, password, userType, email, phone, address, DoB);
-    public AssemblyLineWorker(String name, int UserId, String password, LocalDate dob, String userType) {
-        super(name, UserId, password, dob, userType);
     }
 
     
